f7c16e0a1fcc7ff9390e77136654d06898744e52
abdul kadir  quadri <epzads@gmail.com>
2019-03-18 00:02:49 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,44 @@
 class Dog
+  
   # code goes here
+  attr_accessor :mood
+  attr_reader :name 
+  def initialize(name = "Dog")
+    @name = name 
+    @mood = "nervous"
+end 
+ 
+ 
+    
+
+
+
+
+
+
 end
+
+# let(:dog) { Dog.new("Daisy") }
+
+#   it "can initialize a dog" do
+#     expect(dog).to be_a(Dog)
+#   end
+
+#   it "initializes with a name" do
+#     expect(dog.name).to eq("Daisy")
+#   end
+
+#   it "can't change its name" do
+#     expect { dog.name = "Fido" }.to raise_error NoMethodError
+#   end
+
+#   it "initializes with a nervous mood" do
+#     expect(dog.mood).to eq("nervous")
+#   end
+
+#   it "can change its mood" do
+#     dog.mood = "hungry"
+#     expect(dog.mood).to eq("hungry")
+#   end
+
+# end
--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,44 @@
 class Fish
   # code goes here
+
+  # code goes here
+  attr_accessor :mood
+  attr_reader :name 
+  def initialize(name = "Fish")
+    @name = name 
+    @mood = "nervous"
+end 
+ 
+ 
+    
+
+
+
+
+
 end
+
+# let(:fish) { Fish.new("Nemo") }
+
+#   it "can initialize a fish" do
+#     expect(fish).to be_a(Fish)
+#   end
+
+#   it "initializes with a name" do
+#     expect(fish.name).to eq("Nemo")
+#   end
+
+#   it "can't change its name" do
+#     expect { fish.name = "Goldy" }.to raise_error NoMethodError
+#   end
+
+#   it "initializes with a nervous mood" do
+#     expect(fish.mood).to eq("nervous")
+#   end
+
+#   it "can change its mood" do
+#     fish.mood = "excited"
+#     expect(fish.mood).to eq("excited")
+#   end
+
+# end
